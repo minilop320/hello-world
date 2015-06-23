595d07cd1b32ee98324e971bc24a5d6d685468e9
minilop320 <minilop320@gmail.com>
2015-06-23 22:25:55 +0800

    init

---
+++ testPython/src/TestMoudle.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015/6/16
+
+@author: laura_lu
+'''
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    b = 1
+    c = 2
+    d = 3
+    y = 5
+    return y + a +b+c
+  
+print add(1,2)
+print addFixedValue(1)
